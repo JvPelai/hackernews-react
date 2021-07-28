bbee7d640298727e379a9efb8111deb63ab98c2e
JvPelai <jv.pelai@gmail.com>
2021-07-28 16:05:17 -0300

    search with lazyQuery

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,9 @@ import "../styles/App.css";
 import CreateLink from "./CreateLink";
 import Header from "./Header";
 import LinkList from "./LinkList";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Login from "./Login";
+import Search from "./Search";
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       <Header />
       <div className="ph3 pv1 background-gray">
         <Switch>
+          <Route exact path="/" render={() => <Redirect to="/new/1" />} />
           <Route exact path="/create" component={CreateLink} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/" component={LinkList} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </div>
     </div>
